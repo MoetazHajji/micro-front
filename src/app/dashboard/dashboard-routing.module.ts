ed8df917e63097b9aaece6d239107500e4f52b2f
siwar atiya <siwar.atiya@esprit.tn>
2023-10-22 16:57:09 +0200

    resolving conflict

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AdminLayoutComponent} from "./admin-layout/admin-layout.component";
 import {ProductComponent} from "./product/product.component";
+import { SampleComponent } from './sample/sample.component';
+import {TestComponent} from "./test/test.component";
 
 const routes: Routes = [
   {path:'',component:ProductComponent},
@@ -9,6 +11,18 @@ const routes: Routes = [
     path:'product',component:AdminLayoutComponent,
     children: [{
       path:'',component:ProductComponent
+    }]
+  },
+  {
+    path:'sample',component:AdminLayoutComponent,
+    children: [{
+      path:'',component:SampleComponent
+    }]
+  },
+  {
+    path:'test',component:AdminLayoutComponent,
+    children: [{
+      path:'',component:TestComponent
     }]
   }
 ];
